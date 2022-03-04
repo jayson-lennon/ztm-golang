66f81963b02bac92cb7ddf01f4f30aedd174acb0
Jayson Lennon <jayson@jaysonlennon.dev>
2022-03-03 18:36:26 -0800

    Finish mailinglist demo

---
+++ src/projects/mailinglist/go.mod
@@ -0,0 +1,19 @@
+module mailinglist
+
+go 1.18
+
+require (
+	github.com/alexflint/go-arg v1.4.3
+	github.com/mattn/go-sqlite3 v1.14.12
+	google.golang.org/grpc v1.44.0
+	google.golang.org/protobuf v1.27.1
+)
+
+require (
+	github.com/alexflint/go-scalar v1.1.0 // indirect
+	github.com/golang/protobuf v1.5.0 // indirect
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
+	golang.org/x/text v0.3.0 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+)
---
+++ src/projects/mailinglist/server/server.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"mailinglist/grpcapi"
+	"mailinglist/jsonapi"
+	"mailinglist/mdb"
+	"sync"
+
+	"github.com/alexflint/go-arg"
+)
+
+var args struct {
+	DbPath   string `arg:"env:MAILINGLIST_DB"`
+	BindJson string `arg:"env:MAILINGLIST_BIND_JSON"`
+	BindGrpc string `arg:"env:MAILINGLIST_BIND_GRPC"`
+}
+
+func main() {
+	arg.MustParse(&args)
+
+	if args.DbPath == "" {
+		args.DbPath = "list.db"
+	}
+	if args.BindJson == "" {
+		args.BindJson = ":8080"
+	}
+	if args.BindGrpc == "" {
+		args.BindGrpc = ":8081"
+	}
+
+	log.Printf("using database '%v'\n", args.DbPath)
+	db, err := sql.Open("sqlite3", args.DbPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	mdb.TryCreate(db)
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		log.Printf("starting JSON API server...\n")
+		jsonapi.Serve(db, args.BindJson)
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func() {
+		log.Printf("starting gRPC API server...\n")
+		grpcapi.Serve(db, args.BindGrpc)
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
