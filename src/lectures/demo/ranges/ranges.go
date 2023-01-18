55271df767242ac7838ba7a72fd512e1dee714b3
saeedkaz <saeed.kazemy@gmail.com>
2023-01-18 06:15:14 +0330

    range

--- src/lectures/demo/ranges/ranges.go
+++ src/lectures/demo/ranges/ranges.go
@@ -3,4 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	slice := []string{"saeed", "nirvana", "#"}
+	for i, element := range slice {
+		fmt.Println(i, element, ":")
+		for _, ch := range element {
+			fmt.Printf("    %q", ch)
+		}
+		fmt.Println()
+
+	}
+
 }
