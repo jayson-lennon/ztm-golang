f10fbf88976a833892dc36ab16f1ca1509194501
saeedkaz <saeed.kazemy@gmail.com>
2023-01-18 05:13:45 +0330

    slices done

--- src/lectures/demo/slices/slices.go
+++ src/lectures/demo/slices/slices.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+func printSlice(title string, slice []string) {
+	fmt.Println()
+	fmt.Println("---", title, "---")
+	for i := 0; i < len(slice); i++ {
+		element := slice[i]
+		fmt.Println(element)
+	}
+}
+
 func main() {
+	route := []string{"home", "work", "gym"}
+	printSlice("route1", route)
+	route = append(route, "bike shop")
+	printSlice("route2", route)
+	fmt.Println("home visited")
+	fmt.Println("work visited")
+	route = route[2:]
+	printSlice("new route", route)
 
 }
--- src/lectures/exercise/slices/slices.go
+++ src/lectures/exercise/slices/slices.go
@@ -2,21 +2,39 @@
 //  Create a program to manage parts on an assembly line.
 //
 //--Requirements:
-//* Using a slice, create an assembly line that contains type Part
-//* Create a function to print out the contents of the assembly line
-//* Perform the following:
-//  - Create an assembly line having any three parts
-//  - Add two new parts to the line
-//  - Slice the assembly line so it contains only the two new parts
-//  - Print out the contents of the assembly line at each step
-//--Notes:
-//* Your program output should list 3 parts, then 5 parts, then 2 parts
 
 package main
 
 import "fmt"
 
+//* Create a function to print out the contents of the assembly line
+
 type Part string
 
+func printAssemblyline(name string, slice []string) {
+	fmt.Println("-------", name, "----------")
+	for i := 0; i < len(slice); i++ {
+		element := slice[i]
+		fmt.Println(element)
+	}
+
+}
+
 func main() {
+	//* Using a slice, create an assembly line that contains type Part
+	//* Perform the following:
+	//  - Create an assembly line having any three parts
+	assemblyline := []string{"part1", "part2", "part3"}
+	printAssemblyline("line1", assemblyline)
+	//  - Add two new parts to the line
+	assemblyline = append(assemblyline, "part 4", "part 5")
+	printAssemblyline("line 2", assemblyline)
+	assemblyline = assemblyline[3:]
+	printAssemblyline("line 3", assemblyline)
+
+	//  - Slice the assembly line so it contains only the two new parts
+	//  - Print out the contents of the assembly line at each step
+	//--Notes:
+	//* Your program output should list 3 parts, then 5 parts, then 2 parts
+
 }
