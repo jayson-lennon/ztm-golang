57b11e1cfc0bf59114a70075f5e9ef43c4716953
saeedkaz <saeed.kazemy@gmail.com>
2023-01-20 02:06:05 +0330

    add len to range demo

--- src/lectures/demo/ranges/ranges.go
+++ src/lectures/demo/ranges/ranges.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	slice := []string{"saeed", "nirvana", "#"}
 	for i, element := range slice {
-		fmt.Println(i, element, ":")
+		fmt.Println(i, "length:", len(element), element, ":")
 		for _, ch := range element {
 			fmt.Printf("    %q", ch)
 		}
