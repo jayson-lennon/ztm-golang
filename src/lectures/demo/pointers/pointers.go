49411d5d820a2377ad6f0814caa6c4ad82fc95b6
saeedkaz <saeed.kazemy@gmail.com>
2023-01-20 14:22:50 +0330

    pointer demo

--- src/lectures/demo/pointers/pointers.go
+++ src/lectures/demo/pointers/pointers.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
+type Counter struct {
+	hits int
+}
+
+func increment(counter *Counter) {
+	counter.hits++
+	fmt.Println("counter is:", counter)
+}
+func replace(old *string, new string, counter *Counter) {
+	*old = new
+	increment(counter)
+}
+
 func main() {
+	counter := Counter{}
+	hello := "hello"
+	world := "world"
+	//hi := "hi"
+	fmt.Println(hello, world)
+	replace(&hello, "Hi", &counter)
+	fmt.Println(hello, world)
+
+	phrase := []string{hello, world}
+	fmt.Println(phrase)
+	replace(&phrase[1], "go", &counter)
+	fmt.Println(phrase)
 
 }
