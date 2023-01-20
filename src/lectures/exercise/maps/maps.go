61e49a23feaf5b4331797eec4cf41defbc37b30d
saeedkaz <saeed.kazemy@gmail.com>
2023-01-20 11:20:30 +0330

    range ex

--- src/lectures/exercise/maps/maps.go
+++ src/lectures/exercise/maps/maps.go
@@ -4,19 +4,6 @@
 //  in the `servers` slice.
 //
 //--Requirements:
-//* Create a function to print server status displaying:
-//  - number of servers
-//  - number of servers for each status (Online, Offline, Maintenance, Retired)
-//* Create a map using the server names as the key and the server status
-//  as the value
-//* Set all of the server statuses to `Online` when creating the map
-//* After creating the map, perform the following actions:
-//  - call display server info function
-//  - change server status of `darkstar` to `Retired`
-//  - change server status of `aiur` to `Offline`
-//  - call display server info function
-//  - change server status of all servers to `Maintenance`
-//  - call display server info function
 
 package main
 
@@ -29,6 +16,42 @@ const (
 	Retired     = 3
 )
 
+// * Create a function to print server status displaying:
+//   - number of servers
+//   - number of servers for each status (Online, Offline, Maintenance, Retired)
+func printSrvSts(servermap map[string]int) {
+	fmt.Println("Number of servers are:", len(servermap))
+	serverStat := make(map[int]int)
+	for _, srvSts := range servermap {
+		serverStat[srvSts]++
+	}
+	fmt.Println("Servers status are:", serverStat)
+
+}
+
 func main() {
 	servers := []string{"darkstar", "aiur", "omicron", "w359", "baseline"}
+
+	//* Create a map using the server names as the key and the server status
+	//  as the value
+	//* Set all of the server statuses to `Online` when creating the map
+	servermap := make(map[string]int)
+	for _, srvName := range servers {
+		servermap[srvName] = Online
+	}
+	printSrvSts(servermap)
+	// * After creating the map, perform the following actions:
+	//   - call display server info function
+	//   - change server status of `darkstar` to `Retired`
+	servermap["darkstar"] = Retired
+	//   - change server status of `aiur` to `Offline`
+	servermap["aiur"] = Offline
+	printSrvSts(servermap)
+	//   - call display server info function
+	//   - change server status of all servers to `Maintenance`
+	for _, srvName := range servers {
+		servermap[srvName] = Maintenance
+	}
+	printSrvSts(servermap)
+	//   - call display server info function
 }
