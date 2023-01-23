ebb5db7f780d2f4bf7f2e9d8747026a8eb937bfe
Jayson Lennon <jayson@jaysonlennon.dev>
2023-01-23 09:46:22 -0800

    Fix spelling

--- src/lectures/exercise/sr-dice/dice.go
+++ src/lectures/exercise/sr-dice/dice.go
@@ -5,7 +5,7 @@
 //
 //--Requirements:
 //* Print the sum of the dice roll
-//* Print additional information in these cirsumstances:
+//* Print additional information in these circumstances:
 //  - "Snake eyes": when the total roll is 2, and total dice is 2
 //  - "Lucky 7": when the total roll is 7
 //  - "Even": when the total roll is even
